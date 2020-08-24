967701b013cd9259eb215f2e4bd424e180402699
yoonjong.park <68855211+yoonjong-park@users.noreply.github.com>
2020-08-24 12:22:58 +0900

    ADD Function Drawing
    const ctx = canvas.getContext("2d");
    
    
    ctx.strokeStyle = "#2c2c2c";
    ctx.lineWidth = 2.5; //선의 넓이
    
    
    canvas.width = 700;
    canvas.height = 700;
    
    
    beginPath()
    stroke()

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5; //선의 넓이
 
 let painting = false;
 
@@ -6,18 +13,29 @@ function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function onMouseDown(event) {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting();
-}
+// function onMouseUp(event) {
+//   stopPainting();
+// }
 
 function onMouseLeave(event) {
   stopPainting();
@@ -25,7 +43,7 @@ function onMouseLeave(event) {
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
